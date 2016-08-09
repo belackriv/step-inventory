d06bb73085fd96550adc7641581a8b15163a2c1d
belackriv <belackriv@gmail.com>
2016-08-08 20:00:01 -0500

    deployer update

---
+++ create_db_user.sql
@@ -0,0 +1,3 @@
+ CREATE DATABASE `step-inventory`;
+ CREATE USER 'step-inventory'@localhost IDENTIFIED BY 'step-inventory';
+ GRANT ALL PRIVILEGES ON `step-inventory`.* TO 'step-inventory'@'localhost';
