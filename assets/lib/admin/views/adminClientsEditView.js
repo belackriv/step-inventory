991cdb4db6415d62553798c94a2fe5ddab1355d0
belackriv <belackriv@gmail.com>
2017-02-12 16:51:49 -0600

    clients can have contacts

--- assets/lib/admin/views/adminClientsEditView.js
+++ assets/lib/admin/views/adminClientsEditView.js
@@ -7,6 +7,10 @@ import Marionette from 'marionette';
 
 import viewTpl from  "./adminClientsEditView.hbs!";
 
+import ContactModel from 'lib/common/models/contactModel.js';
+import AdminContactsEditView from './adminContactsEditView.js';
+import ContactListView from './contactListView.js';
+
 export default Marionette.View.extend({
   template: viewTpl,
   behaviors: {
@@ -15,10 +19,43 @@ export default Marionette.View.extend({
     'SetNotSynced': {},
     'SaveCancelDelete': {},
   },
+  regions: {
+    'contacts': '[data-ui-name="contacts"]'
+  },
   ui: {
     'nameInput': 'input[name="name"]',
+    'addContactButton': 'button[data-ui-name="addContact"]'
+  },
+  events: {
+    'click @ui.addContactButton': 'openAddContactDialog'
   },
   bindings: {
     '@ui.nameInput': 'name',
   },
+  onRender(){
+    this.showChildView('contacts', new ContactListView({
+      collection: this.model.get('contacts'),
+    }));
+  },
+  openAddContactDialog(event){
+    event.preventDefault();
+    var options = {
+      title: 'Add Contact',
+      width: '400px'
+    };
+    let contact = ContactModel.findOrCreate({
+      client: this.model
+    });
+    let view = new AdminContactsEditView({
+      model: contact,
+      postDelete(){
+        Radio.channel('dialog').trigger('close');
+      }
+    });
+    this.listenTo(view, 'show:list', ()=>{
+      Radio.channel('dialog').trigger('close');
+    });
+    Radio.channel('dialog').trigger('close');
+    Radio.channel('dialog').trigger('open', view, options);
+  }
 });
